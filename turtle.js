7333aa6d44b68e6a07633cb5823b392a399b50d5
David Ko <dkoflow@gmail.com>
2018-11-22 21:20:45 -0800

    Added print() method to complete my code

--- turtle.js
+++ turtle.js
@@ -64,10 +64,13 @@ class Turtle {
      }
    }  
 
-   myPrint(){
+   print(){
      let allCoordinates = this.coordinates;
-     let finalArray = [];
-     function toSortArray (x){
+     let maxCoordinates = [];
+     let board = [];
+     let boardString = '';
+ 
+     function findMax (x){
        let biggestX = 0
        let biggestY = 0
        for (let i = 0; i < x.length; i++){
@@ -75,19 +78,48 @@ class Turtle {
             biggestX = x[i][0]
           }
          }
- 
        for (let j = 0; j < x.length; j++){
           if(x[j][1] > biggestY){
             biggestY = x[j][1]
           }
          }
       
-        finalArray.push([biggestX, biggestY])
+        maxCoordinates.push([biggestX, biggestY])
         
      }
-      toSortArray(allCoordinates)
-      return(finalArray)
-   }
+      findMax(allCoordinates)
+      // return maxCoordinates
+      
+     
+     function makeBoard(width, height){
+       for (let i = 0; i<=height; i++){
+         for (let j = 0; j<=width; j++){
+          board.push([j, i])
+         }
+       }
+     }
+      makeBoard(maxCoordinates[0][0], maxCoordinates[0][1])
+      // return board
+      function printBoard(board, width){
+          // console.log(board.length)
+          for(let boardPoint = 0; boardPoint < board.length; boardPoint++ ){
+          let currentPoint = board[boardPoint];
+          let isTurtleStep = allCoordinates.find((step) => {
+              // console.log(step[0], 'step')
+              // console.log(currentPoint[0], 'currentPoint')
+              return (step[0] === currentPoint[0]) && (step[1] === currentPoint[1])
+            })
+            if (currentPoint[0] === width){  // width - 1?
+              boardString += isTurtleStep ? '* \n' : '0 \n';
+            } else {
+              boardString += isTurtleStep ? '*' : '0';
+            }
+          } 
+          // console.log(boardString)
+        }
+        printBoard(board, maxCoordinates[0][0]) 
+        console.log(boardString) 
+     }
   
 }
 
