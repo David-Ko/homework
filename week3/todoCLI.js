bbb674cd2f46683b255016140d4a9a07d48aa4c0
David Ko <dkoflow@gmail.com>
2018-11-23 22:25:14 -0800

    added a barebone structure

--- week3/todoCLI.js
+++ week3/todoCLI.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question(
+    `Welcome to ToDo CLI!\n--------------------\n(v) View ∙ (n) New ∙ (cX) Complete ∙ (dX) Delete ∙ (q) Quit\n>`, 
+    ()=>{
+        console.log('hi')
+        rl.close();
+    }
+
+);
