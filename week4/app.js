1dfc0a93df24261f1685c629748e96fe9734269e
David Ko <dkoflow@gmail.com>
2018-12-07 13:27:21 -0800

    cleaned up app.js

--- week4/app.js
+++ week4/app.js
@@ -4,33 +4,22 @@ const logger = require("morgan");
 app.use(logger("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}))
-const methodOverride = require("method-override") //KEEP FORGETTING THIS!!!!
-
+const methodOverride = require("method-override")
 
 const baseRouter = require("./routes/base.js")
 app.use('/', baseRouter);
 
-// app.get("/", (req, res)=>{
-//     res.render('homePage')
-// });
-
-app.use(                                    //KEEP FORGETTING THIS!!!!
-    methodOverride((req, res) => {          //KEEP FORGETTING TO npm i method-override!!!
+app.use(                                    
+    methodOverride((req, res) => {
       if (req.body && req.body._method) {
         const method = req.body._method;
         return method;
       }
-    }),
-  );
+  }),
+);
 
 const cohortsRouter = require("./routes/cohorts.js");
 app.use('/cohorts', cohortsRouter);
-//now, go to routes folder, go to cohorts.js.  All the paths are now
-//pre-pended with /cohorts
-
-
-
-
 
 const PORT = 5002;
 const HOST = "localhost";
