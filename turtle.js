cca868fd44ef2df4c02cc26a80405418c8ec9c8d
David-Ko <43652043+David-Ko@users.noreply.github.com>
2018-12-21 16:42:14 -0800

    Clean up white spaces and implemented chaining

--- turtle.js
+++ turtle.js
@@ -1,44 +1,42 @@
 // worked on it on Monday Nov 19, 2018 at 930pm
 class Turtle {
-
-    constructor (x, y){
+   constructor (x, y) {
         this.x = x;
         this.y = y;
         this.face = 'east';
         this.coordinates = [this.x, this.y];
-    }
-
-   allPoints(){
-       
+   };
+    
+   allPoints() {
          return this.coordinates
-        
-   }
+   };
    
    forward(steps) {
-
-     if (this.face === 'east'){
+     if (this.face === 'east') {
           for (let i = 0; i < steps; i++) {
           this.x += 1;
           this.coordinates.push([this.x, this.y])
-          }
+          };
      } else if (this.face === 'south') {
           for (let i = 0; i < steps; i++) {
           this.y += 1;   
           this.coordinates.push([this.x, this.y])
-          }
+          };
      } else if (this.face === 'west') {
           for (let i = 0; i < steps; i++) {
           this.x += -1;
           this.coordinates.push([this.x, this.y])
-          }
+          };
      } else if (this.face === 'north') {
           for (let i = 0; i < steps; i++) {
           this.y += -1
           this.coordinates.push([this.x, this.y])
-          }
-     }
-   }
-   right(){
+          };
+     };
+     return this; 
+   };
+    
+   right() {
         if (this.face === 'east') {
              this.face = 'south'
         } else if (this.face === 'south') {
@@ -47,90 +45,72 @@ class Turtle {
              this.face = 'north'
         } else if (this.face === 'north') {
              this.face = 'east'
-        }
-     }
+        };
+      return this;
+     };
 
-   left(){
-     if (this.face === 'east') {
-          this.face = 'north'
-     } else if (this.face === 'north') {
-          this.face = 'west'
-     } else if (this.face === 'west') {
-          this.face = 'south'
-     } else if (this.face === 'south') {
-          this.face = 'east'
-     }
-   }  
+   left() {
+         if (this.face === 'east') {
+              this.face = 'north'
+         } else if (this.face === 'north') {
+              this.face = 'west'
+         } else if (this.face === 'west') {
+              this.face = 'south'
+         } else if (this.face === 'south') {
+              this.face = 'east'
+         };
+     return this;
+    }; 
 
-   print(){
+   print() {
      let allCoordinates = this.coordinates;
      let maxCoordinates = [];
      let board = [];
      let boardString = '';
  
-     function findMax (x){
+     findMax (coordinates) {
        let biggestX = 0
        let biggestY = 0
-       for (let i = 0; i < x.length; i++){
-          if(x[i][0] > biggestX){
-            biggestX = x[i][0]
+       for (let i = 0; i < coordinates.length; i++){
+          if(coordinates[i][0] > biggestX){
+            biggestX = coordinates[i][0]
           }
-         }
+         };
        for (let j = 0; j < x.length; j++){
           if(x[j][1] > biggestY){
             biggestY = x[j][1]
-          }
-         }
-      
+          };
+         };
         maxCoordinates.push([biggestX, biggestY])
-        
-     }
+     };
       findMax(allCoordinates)
-      // return maxCoordinates
-      
-     
-     function makeBoard(width, height){
+
+     makeBoard (width, height) {
        for (let i = 0; i<=height; i++){
          for (let j = 0; j<=width; j++){
           board.push([j, i])
-         }
-       }
-     }
-      makeBoard(maxCoordinates[0][0], maxCoordinates[0][1])
-      // return board
-      function printBoard(board, width){
-          // console.log(board.length)
+         };
+       };
+     };
+      makeBoard (maxCoordinates[0][0], maxCoordinates[0][1])
+       
+     printBoard (board, width) {
           for(let boardPoint = 0; boardPoint < board.length; boardPoint++ ){
           let currentPoint = board[boardPoint];
           let isTurtleStep = allCoordinates.find((step) => {
-              // console.log(step[0], 'step')
-              // console.log(currentPoint[0], 'currentPoint')
               return (step[0] === currentPoint[0]) && (step[1] === currentPoint[1])
             })
             if (currentPoint[0] === width){  // width - 1?
               boardString += isTurtleStep ? '* \n' : '0 \n';
             } else {
               boardString += isTurtleStep ? '*' : '0';
-            }
-          } 
-          // console.log(boardString)
-        }
+            };
+          } ;
+        };
         printBoard(board, maxCoordinates[0][0]) 
         console.log(boardString) 
-     }
-  
-}
+     };
+};
 
 let Sonic = new Turtle (0,0)
-Sonic
-Sonic.forward(5)
-Sonic.right()
-Sonic.forward(5)
-Sonic.right()
-Sonic.forward(5)
-Sonic.right()
-Sonic.forward(5)
-Sonic.print()
-
-
- 
+Sonic.forward(5).right().forward(5).right().forward(5).right().forward(5).print()
